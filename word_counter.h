486480c51c2eb5b0f01b43d410858fd01d92adf4
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-04-06 23:12:12 +0200

    Added basic methods of WordCounter class to header file

--- word_counter.h
+++ word_counter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <initializer_list>
 #include "entry.h"
 
 class WordCounter
@@ -7,8 +8,23 @@ class WordCounter
 private:
     std::vector<Entry> counter;
 public:
-    WordCounter();
+    WordCounter() = default;
+    WordCounter(std::initializer_list<Entry> entry_lst);
 
     void addWord(std::string& word);
+    void addWords(std::istream& is);
     void clear();
+
+    const Entry& operator[](const std::string& value) const;
+    Entry& operator[](const std::string& value);
+
+    void operator+=(const std::string& value);
+
+    friend std::istream& operator>>(std::istream& is, WordCounter& counter);
+
+    class LexIterator;
+    class FreqIterator;
 };
+
+std::ostream& operator<<(std::ostream& os, const WordCounter& counter);
+
