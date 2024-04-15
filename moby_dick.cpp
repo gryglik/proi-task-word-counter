1539b0736386caac94df188a6d398b24446e082e
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-04-16 01:00:08 +0200

    implemented moby_dick performance measuring script

--- moby_dick.cpp
+++ moby_dick.cpp
@@ -1,7 +1,28 @@
 #include "word_counter.h"
 #include <fstream>
+#include <chrono>
 
 int main()
 {
+    WordCounter wc;
+    std::ifstream in;
+    // Create time
+    in.open("moby_dick.txt");
+    auto start_time = std::chrono::high_resolution_clock::now();
+    wc.addWords(in);
+    auto end_time = std::chrono::high_resolution_clock::now();
+    in.close();
+
+    std::cout << "Create time: "
+    << (end_time - start_time) / std::chrono::milliseconds(1) << " ms.\n";
+
+    //Lexicographical iteration time
+    start_time = std::chrono::high_resolution_clock::now();
+    for (auto it = wc.lexBegin(); it != wc.lexEnd(); it++) {}
+    end_time = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Lexicographical iteration time: "
+    << (end_time - start_time) / std::chrono::milliseconds(1) << " ms.\n";
+
     return 0;
 }
